be03a2955eff20a2fc7823fa543c31b56ecd3ec9
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-12-09 15:27:40 -0800

    passing get / method

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,12 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
-
+  it('handles a get request for path "/" by sending "hi" to client side', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('Hi');
+      });
+  });
 });
+
